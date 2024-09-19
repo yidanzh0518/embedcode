66c8400271fbef96e939888f7d9be3a3346dc2a1
yidanzh0518 <yidan.zhang@utah.edu>
2024-09-19 13:12:49 -0600

    Lab5 done @gvegayon @chipmanj https://github.com/UofUEpiBio/PHS7045-advanced-programming/issues/29

--- PHS7045-HW/lab5.cpp
+++ PHS7045-HW/lab5.cpp
@@ -8,9 +8,18 @@ int main () {
   }
   
   std::cout << "Q2:Choose" << std::endl;
-  for (int i = 1; i <= 10; i++) {
+  for (int i = 1; i < 11; i++) {
     std::cout << b.choose(10, i) << std::endl;
     
   }
   
+  std::cout << "Q3:implement the binom" << std::endl;
+  for (int i = 0; i < 11; i++) {
+    std::cout << b.dbinom(i) << std::endl;
+  }
+  
+  std::cout << "Q4:Print" << std::endl;
+  for (int i = 0; i < 11; i++) {
+   b.print(i);
+  }
 }

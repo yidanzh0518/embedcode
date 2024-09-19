659345b139a3418f3f6ea0af5a79b3523cfd9e86
yidanzh0518 <yidan.zhang@utah.edu>
2024-09-19 12:40:06 -0600

    exercise 2

--- PHS7045-HW/lab5.cpp
+++ PHS7045-HW/lab5.cpp
@@ -2,7 +2,15 @@
 
 int main () {
   Binom b(10, 0.5);
-  for (int i = 0; i < 10; i++) {
+  std::cout << "Q1:Factorials" << std::endl;
+  for (int i = 1; i <= 10; i++) {
     std::cout << b.factorial(i) << std::endl;
   }
+  
+  std::cout << "Q2:Choose" << std::endl;
+  for (int i = 1; i <= 10; i++) {
+    std::cout << b.choose(10, i) << std::endl;
+    
+  }
+  
 }
